0289561df7e190530fd01f903b5d6595f47f572d
piqui <lmaosaurus@gmail.com>
2013-11-17 17:23:00 -0600

    sanitize url

--- routes/lunch.js
+++ routes/lunch.js
@@ -1,7 +1,23 @@
 _ = require('underscore');
 
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g, '');
+  };
+}
+pattern = /^http(s)?:\/\//i;
+
+var sanitize_url = function(teststring){
+  teststring = teststring.trim();
+  if(teststring.search(pattern) == -1){
+    teststring = 'http://' + teststring;
+  }
+  return teststring;
+}
+
+
 exports.post_lunch = function(req, res, next) {
-  lunch = new Lunch({title: req.body.title, menu_url: req.body.menu_url});
+  lunch = new Lunch({title: req.body.title, menu_url: sanitize_url(req.body.menu_url)});
   console.log('my lunch', lunch)
   req.session.lunch = lunch.attributes;
   lunch.save();
