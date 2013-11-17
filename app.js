bd33e1500811878ab2200ec89c06fd5ea0bae2c8
danschumann <dan.schumann@gmail.com>
2013-11-17 14:11:59 -0600

    lunches will save

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret:'asdfdsfajkfadsjf ksjdfkadsfasdfkjasldfjalsdfj'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/lunches.js
+++
@@ -1,8 +0,0 @@
-_ = require('underscore');
-
-exports.post_lunch = function(req, res, next) {
-  lunch = new Lunch({title: req.body.title});
-  req.session.lunch = lunch;
-  lunch.save();
-  res.redirect('/newlunch');
-};
