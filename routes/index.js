cb601bd45caf9214a8c1e065351138133581a19c
mdkrajnak <mdkrajnak@gmail.com>
2013-11-17 14:06:16 -0600

    Initial "new lunch" page.

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.newlunch = function(req, res) {
+  res.render('newlunch', { title: "title", lunchid: "newlunch" });
+};
