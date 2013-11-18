d2b0062f75d4e726ec1fa2c5ca6d7ee94f767d8f
danschumann <dan.schumann@gmail.com>
2013-11-17 18:29:04 -0600

    edit orders

--- routes/order.js
+++ routes/order.js
@@ -28,3 +28,12 @@ exports.show = function(req, res) {
   console.log('show order', order);
   res.render('orders/edit', {lunch_id: req.params.lunch_id, order: order} );
 };
+
+exports.update = function(req, res) {
+  order = Order.find(req.params.order_id);
+  _.extend(order, _.pick(req.body, 'user', 'description'));
+  (new Order(order)).save()
+
+  console.log('show order', order);
+  res.redirect('/lunches/' + req.params.lunch_id);
+};
