f666502be70720df53d73ace2c8c30716b87d433
mdkrajnak <mdkrajnak@gmail.com>
2013-11-17 18:07:21 -0600

    Fix merge conficts

--- routes/order.js
+++ routes/order.js
@@ -2,16 +2,9 @@ _ = require('underscore');
 
 exports.create = function(req, res, next) {
 
-<<<<<<< HEAD
   user = req.body.user;
   description = req.body.description;
   lunch_id = req.body.lunch_id;
-=======
-  item_number = req.body.item_number;
-  name = req.body.name;
-  price = req.body.price;
-  lunch_id = req.params.lunch_id;
->>>>>>> c9819647c4841bc9524e91a8f3b2ac6fa6bf1d5e
 
   order = new Order({
 	user: user,
